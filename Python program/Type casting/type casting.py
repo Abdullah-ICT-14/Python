edd6e782d09c5acbf1f6e970667152acefa3fda9
Abdullah-ICT-14 <mdabdullah01728@gmail.com>
2021-02-14 10:57:30 +0600

    Type casting

---
+++ Python program/Type casting/type casting.py
@@ -0,0 +1,8 @@
+c=input()
+d=input()
+d=int(c)+int(d)
+print(d)
+
+c=int(input())
+d=int(input())
+print(c+d)
---
+++ Python program/Type casting/typeConverter.py
@@ -0,0 +1,13 @@
+xr=10 #int
+yr=20.5 #float
+zr=1j #complex
+
+x=float(xr)
+y=int(yr)
+z=complex(xr)
+w=str(xr)
+#z=float(zr)#You cannot convert complex numbers into another number type.
+print(x)
+print(y)
+print(z)
+print(w)
