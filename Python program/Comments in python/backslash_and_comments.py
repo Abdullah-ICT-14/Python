88ecbd77875ead63a9c1a71c039b3ebec7f212bd
Abdullah-ICT-14 <mdabdullah01728@gmail.com>
2021-02-14 07:55:51 +0600

    Comments in python

---
+++ Python program/Comments in python/backslash_and_comments.py
@@ -0,0 +1,17 @@
+#this is single line comment
+print('This is single line comments')
+"""
+this 
+is 
+multiline
+comments
+"""
+
+
+print('abdullah')
+print('01728985125')
+#backslash
+print('rasid \n01626850160')
+
+print('abdullah \t 01728985125')
+print('\'abdullah\'')
---
+++ Python program/Comments in python/comment.py
@@ -0,0 +1,19 @@
+
+"""
+in python comment is start with #
+print('hello world') #can also be used to
+prevent Python from executing code:
+"""
+"""
+Since Python will ignore string literals that are not assigned to a variable, you can
+add a multiline string (triple quotes) in your code, and place your comment inside it:
+
+"""
+print('hello world') #comment can be placed at the end of the lline
+
+def myfunc():
+     kar
+    kar=5
+    print(kar)
+myfunc()
+print(kar) #program not execute because kar is a local variable
