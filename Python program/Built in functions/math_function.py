c458a8008973709fcee8a2faeea8498dd2fc565d
Abdullah-ICT-14 <mdabdullah01728@gmail.com>
2021-02-17 21:00:57 +0600

    Built in functions

---
+++ Python program/Built in functions/math_function.py
@@ -0,0 +1,19 @@
+from  math import *
+a=max(1,2,4,5,6,8,9)
+b=min(1,2,4,5,6,8,9)
+c=a+b
+d=sqrt(c)
+e=sin(d)
+print(e)
+e=floor(e)
+print(e)
+e=ceil(e)
+print(pow(e,2))
+print(exp(e))
+
+aa=20
+bb=30
+print('sum is:',aa,'+',bb,'=',aa+bb)
+
+print('abdullah',end=' ')
+print('khalid')
---
+++ Python program/Built in functions/range_function.py
@@ -0,0 +1,16 @@
+a=list(range(100))
+b=tuple(range(10,20))
+c=a[2]+b[3]
+print(a[2])
+print(b[3])
+print(c)
+"""
+a=int(input())
+b=int(input())
+c=int(input())
+for x in range(a,b,c):
+    print(x)
+
+"""
+cc=list(int(input('Enter list:\n')))
+print(cc)
