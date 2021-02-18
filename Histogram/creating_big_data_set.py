b4d6f8fceadea086f7b69a82e4476686641ac95b
Abdullah-ICT-14 <mdabdullah01728@gmail.com>
2021-02-18 20:55:18 +0600

    Histogram

---
+++ Histogram/creating_big_data_set.py
@@ -0,0 +1,3 @@
+import numpy
+x=numpy.random.uniform(0.0,1.0,100)
+print(x,end=' ')
---
+++ Histogram/histogram.py
@@ -0,0 +1,6 @@
+
+import numpy
+import matplotlib.pyplot as plt
+x = numpy.random.uniform(0.0, 5.0, 1000)
+plt.hist(x, 5)
+plt.show()
